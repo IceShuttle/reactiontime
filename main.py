43e32ad0bec6a0086e18360cc9eeb4001c1b2ac9
IceShuttle <IceShuttle@Debian>
2024-05-10 23:37:10 +0530

    images_resized_and_click_show_implemented

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sys
 import time
 import random
 
+from pygame.math import clamp
+
 #Data Collection Initialize
 data = []
 
@@ -12,8 +14,8 @@ data = []
 pygame.init()
 
 # Set the dimensions of the window
-WINDOW_WIDTH = 1920
-WINDOW_HEIGHT = 1080
+WINDOW_WIDTH = 1600
+WINDOW_HEIGHT = 800
 
 # Set the colors
 WHITE = (255, 255, 255)
@@ -39,6 +41,20 @@ def save_data(file_path):
         writer.writerow(("Name","Time(s)"))
         writer.writerows(data)
 
+def wait_for_mouse(cross, cross_x, cross_y,time_limit):
+    start = time.time()
+    pressed = False
+    while time.time()-start<time_limit and not pressed:
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mousex,mousey = pygame.mouse.get_pos()
+                rad = 200
+                if abs(mousex-cross_x)<rad and abs(mousey-cross_y)<rad:
+                    pressed=True
+
+
+
+
 # Main function to run the experiment
 def main():
     img_paths = ["images/"+name for name in listdir("./images/")]
@@ -52,27 +68,23 @@ def main():
     
     START = time.time()
     for i,img in enumerate(images):
-        cross_x = WINDOW_WIDTH*random.random()*.8
-        cross_y = WINDOW_HEIGHT*random.random()*.8
+        cross_x = int(clamp(WINDOW_WIDTH*random.random(),WINDOW_WIDTH*.2,WINDOW_WIDTH*.8))
+        cross_y = int(clamp(WINDOW_HEIGHT*random.random(),WINDOW_HEIGHT*.2,WINDOW_HEIGHT*.8))
         screen.fill(WHITE)
         screen.blit(cross,(cross_x,cross_y))
         pygame.display.flip()
-        start = time.time()
-        pressed = False
-        while time.time()-start<2 and not pressed:
-            for event in pygame.event.get():
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    mousex,mousey = pygame.mouse.get_pos()
-                    rad = 100
-                    if abs(mousex-cross_x)<rad and abs(mousey-cross_y)<rad:
-                        pressed=True
-                        screen.fill(WHITE)
-                        screen.blit(cross,(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-                        pygame.display.flip()
-        time.sleep(2)
+
+        wait_for_mouse(cross,cross_x,cross_y,2)
+
         screen.fill(WHITE)
-        screen.blit(img, (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
-        # screen.blit(img, (0,0))
+        screen.blit(cross,(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+        pygame.display.flip()
+
+        wait_for_mouse(cross,WINDOW_WIDTH/2, WINDOW_HEIGHT/2,4)
+
+        screen.fill(WHITE)
+        size = img.get_rect().size
+        screen.blit(img, (WINDOW_WIDTH/2-size[0]/2, WINDOW_HEIGHT/2-size[1]/2))
         pygame.display.flip()
         data.append(("DispStart",time.time()-START))
         time.sleep(3)
