5bd829af26980a5e428bad894d5cc25933e0a58c
IceShuttle <IceShuttle@Debian>
2024-05-10 20:04:48 +0530

    attention_grabber_implemented

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from os import listdir
+from os import listdir, times
 import csv
 import pygame
 import sys
@@ -12,8 +12,8 @@ data = []
 pygame.init()
 
 # Set the dimensions of the window
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 1920
+WINDOW_HEIGHT = 1080
 
 # Set the colors
 WHITE = (255, 255, 255)
@@ -42,6 +42,7 @@ def save_data(file_path):
 # Main function to run the experiment
 def main():
     img_paths = ["images/"+name for name in listdir("./images/")]
+    cross = pygame.image.load("cross.png")
     random.shuffle(img_paths)  # Shuffle the order of images
 
     images = []
@@ -49,28 +50,36 @@ def main():
         images.append(pygame.image.load(image_path))
         
     
+    START = time.time()
     for i,img in enumerate(images):
-        screen.blit(img, (0, 0))
+        cross_x = WINDOW_WIDTH*random.random()*.8
+        cross_y = WINDOW_HEIGHT*random.random()*.8
+        screen.fill(WHITE)
+        screen.blit(cross,(cross_x,cross_y))
         pygame.display.flip()
-        pygame.time.wait(2000)
-
-        display_blank()
-        start_time = time.time()
-
-        # Wait for a response from the player
-        while True:
+        start = time.time()
+        pressed = False
+        while time.time()-start<2 and not pressed:
             for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    end_time = time.time()  # Record the end time
-                    reaction_time = end_time - start_time  # Calculate the reaction time
-                    image_name = img_paths[i].removeprefix('images/').removesuffix('.png').removesuffix('.jpg')
-                    data.append((image_name,reaction_time))
-                    break
-            else:
-                continue
-            break
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mousex,mousey = pygame.mouse.get_pos()
+                    rad = 100
+                    if abs(mousex-cross_x)<rad and abs(mousey-cross_y)<rad:
+                        pressed=True
+                        screen.fill(WHITE)
+                        screen.blit(cross,(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+                        pygame.display.flip()
+        time.sleep(2)
+        screen.fill(WHITE)
+        screen.blit(img, (WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+        # screen.blit(img, (0,0))
+        pygame.display.flip()
+        data.append(("DispStart",time.time()-START))
+        time.sleep(3)
+        data.append(("Dispend",time.time()-START))
+    save_data("reaction.csv")
 
-    save_data("data.csv")
+
 
 # Run the main function
 if __name__ == "__main__":
